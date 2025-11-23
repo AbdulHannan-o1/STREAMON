fbb3345bab52e6eeae5b6eef3b48e4924b4a0eb9
Abdul Hannan <shaikhhannan729@gmail.com>
2025-11-24 01:43:51 +0500

    preparing .json for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { ConnectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser"
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ import chatRoute from "./routes/chat.route.js"
 
 const app = express();
 const port  = process.env.PORT
+const __dirname = path.resolve();
 
 app.use(cors({
     origin: "http://localhost:5173",
@@ -25,6 +27,14 @@ app.use(cookieParser())
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
 app.use("/api/chat", chatRoute)
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res)=> {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+    })
+}
 
 
 app.listen(port, ()=> {
