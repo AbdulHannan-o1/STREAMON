130deabfc9f56a97393251b7ddb4fe54df0f476f
Abdul Hannan <shaikhhannan729@gmail.com>
2025-11-25 01:37:08 +0500

    vercel setup

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = import.meta.env.MODE === "deployment" ? "http://localhost:5001/api" : "/api";
+const baseUrl = import.meta.env.MODE === "deployment" ? "http://localhost:5001/api" : import.meta.env.VITE_BACKEND_URL + "/api";
 
 export const axiosInstance = axios.create({
   baseURL: baseUrl,
